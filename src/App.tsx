caf958d288d0c28b7de7adad69d8041c969d0e3f
kuone <kuone@example.com>
2022-10-26 14:42:44 +0900

    Reactの構文に関して、推測してコメントを追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from 'react';
 import { invoke } from '@tauri-apps/api';
 
 const App = () => {
+  // [変数名,setter] で宣言、sette(foo) と呼び出すと、変数に foo が入る、かな？
+  // 変数は <> の所で、{} で括ると参照出来るっぽい。
   const [name, setName] = useState('anonymous');
 
   const [hello, setHello] = useState<string>('');
