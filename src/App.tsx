c6ba2cbd9eb9a4a7e745139f1e1e519a902ed5c1
kuone <kuone@example.com>
2022-10-26 14:38:38 +0900

    Rust の関数の呼び出し

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { invoke } from '@tauri-apps/api';
 
 const App = () => {
   const [name, setName] = useState('anonymous');
+
+  const [hello, setHello] = useState<string>('');
+  useEffect(() => {
+    (async () => {
+      const res = await invoke<string>("greet", { name: "Rust" });
+      setHello(res);
+    })();
+  }, []);
 
   return (
     <>
       <div>Hello, {name}</div>
       <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <br />
+      {hello}
     </>
   );
 }
