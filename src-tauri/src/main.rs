4b51b21d02422e40e14c67a55bfd94e494c1603a
kuone <kuone@example.com>
2022-10-26 14:51:18 +0900

    ファイル取得処理 コピペ

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -12,6 +12,42 @@ fn greet(name: &str) -> String {
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_entries])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+#[derive(Serialize)]
+#[serde(tag = "type")]
+enum Entry {
+  #[serde(rename = "file")]
+  File { name: String, path: String },
+  #[serde(rename = "dir")]
+  Dir { name: String, path: String },
+}
+
+#[tauri::command]
+fn get_entries(path: &str) -> Result<Vec<Entry>, String> {
+  let entries = fs::read_dir(path).map_err(|e| format!("{}", e))?;
+
+  let res = entries
+      .filter_map(|entry| -> Option<Entry> {
+          let entry = entry.ok()?;
+          let name = entry.file_name().to_string_lossy().to_string();
+          let path = entry.path().to_string_lossy().to_string();
+          let type_ = entry.file_type().ok()?;
+
+          if type_.is_dir() {
+              Some(Entry::Dir { name, path })
+          } else if type_.is_file() {
+              Some(Entry::File { name, path })
+          } else {
+              None
+          }
+      })
+      .collect();
+
+  Ok(res)
+}
+
