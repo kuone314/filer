669c355fdce657ee8a688769c1ec11a9bb9a7731
kuone <kuone@example.com>
2022-10-28 18:29:05 +0900

    DataGrid 動いた！

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import { useEffect, useRef, useState } from 'react';
 import { invoke } from '@tauri-apps/api';
 import { homeDir } from '@tauri-apps/api/path';
-import DataGrid from "react-data-grid";
+// import DataGrid from "react-data-grid";
 import React from 'react';
+
+import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+
+
 
 // import { Grid } from "gridjs";
 // import "gridjs/dist/theme/mermaid.css";
@@ -61,16 +65,17 @@ const App = () => {
 
 
   function convert(entries: Entries): string[][] {
-    return entries.map(entry => [entry.name,entry.path]);
+    return entries.map(entry => [entry.name, entry.path]);
   }
 
-  const columns = [
-    { key: "id", name: "ID" },
-    { key: "title", name: "Title" }
+  const columns: GridColDef[] = [
+    { field: 'col1', headerName: 'Column 1', width: 150 },
+    { field: 'col2', headerName: 'Column 2', width: 150 },
   ];
-  const rows = [
-    { id: 0, title: "Example" },
-    { id: 1, title: "Demo" }
+  const rows: GridRowsProp = [
+    { id: 1, col1: 'Hello', col2: 'World' },
+    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+    { id: 3, col1: 'MUI', col2: 'is Amazing' },
   ];
 
   return (
@@ -78,10 +83,10 @@ const App = () => {
       <br />
       <input type="text" value={dir} onChange={e => setDir(e.target.value)} />
       <br />
-      <DataGrid
-        columns={columns}
-        rows={rows}
-      />
+
+      <div style={{ height: 300, width: '100%' }}>
+        <DataGrid rows={rows} columns={columns} />
+      </div>
     </>
   );
 }
