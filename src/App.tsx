79ddc1c4762136a8860417a913a8eafd972f6582
kuone <kuone@example.com>
2022-10-29 10:06:40 +0900

    表示数の指定

--- src/App.tsx
+++ src/App.tsx
@@ -65,6 +65,7 @@ const App = () => {
           columns={columns}
           autoHeight
           onRowDoubleClick={onRowDoubleClick}
+          rowsPerPageOptions={[100]}
         />
       </div>
     </>
