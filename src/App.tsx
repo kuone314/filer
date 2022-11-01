bf2030cafe2ce1ad26403391c4d5858b152f2191
kuone <kuone@example.com>
2022-11-01 19:58:03 +0900

    不要コードの削除

--- src/App.tsx
+++ src/App.tsx
@@ -39,21 +39,6 @@ const App = () => {
     })();
   }, [dir]);
 
-
-  const FileListItem = (entry: Entry) => {
-    if (entry.type === "dir") {
-      return <li key={entry.path} onClick={() => setDir(entry.path)}>{entry.name}</li>;
-    } else {
-      return <li key={entry.path}>{entry.name}</li>;
-    }
-  }
-
-  // entry_list 部分の、html の生成、かな。
-  const entry_list = entries ? <ul>
-    {entries.map(entry => { return FileListItem(entry) })}
-  </ul> : null;
-
-
   const data: IGridProps['source'] = {
     localdata:
       [
